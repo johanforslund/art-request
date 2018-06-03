c6124bb20c24447219d754c1ddc8d84dc770e1ab
johanforslund <johan-forslund@hotmail.com>
2018-06-03 21:02:57 +0200

    contract can now handle multiple submissions and creator can get payback if submitter has not submitted within 3 days

--- ArtRequest.sol
+++ ArtRequest.sol
@@ -1,21 +1,24 @@
 pragma solidity ^0.4.0;
 
+//TO DO: Efter 3 dagar om inte A har accepterat/nekat så kan submitter få sin ether
+
 contract ImageRequest {
     struct Submission {
         string url;
         string email;
         address submitter;
         bool previewApproved;
+        uint deadline;
         bool finalized;
         string[] finals;
     }
 
     address public creator;
-    string title;
-    string description;
-    string imageUrl;
+    string public title;
+    string public description;
+    string public imageUrl;
     uint public reward;
-    bool canCancel;
+    uint public numberOfPreviewApprovals;
     Submission[] public submissions;
 
     modifier restricted() {
@@ -29,7 +32,7 @@ contract ImageRequest {
         imageUrl = _imageUrl;
         creator = msg.sender;
         reward = msg.value;
-        canCancel = true;
+        numberOfPreviewApprovals = 0;
     }
 
     function submitPreview(string url, string email) public {
@@ -40,6 +43,7 @@ contract ImageRequest {
             email: email,
             submitter: msg.sender,
             previewApproved: false,
+            deadline: 0,
             finalized: false,
             finals: finals
         });
@@ -48,6 +52,7 @@ contract ImageRequest {
     }
 
     function submitFinal(uint index, string url) public payable {
+        require(submissions[index].previewApproved);
         if (submissions[index].finals.length == 0) {
             require(msg.value >= 1 ether);
         }
@@ -56,19 +61,32 @@ contract ImageRequest {
 
     function approvePreview(uint index) public restricted {
         submissions[index].previewApproved = true;
-        canCancel = false;
+        submissions[index].deadline = now + 259200;
+        numberOfPreviewApprovals++;
     }
 
     function approveFinal(uint index) public restricted {
         require(submissions[index].previewApproved);
-        submissions[index].submitter.transfer(reward + 1 ether);
+        submissions[index].submitter.transfer(reward);
         submissions[0].finalized = true;
         creator.transfer(1 ether);
     }
 
-    function cancel() public restricted {
-        require(canCancel);
+    function cancelApproval(uint index) public restricted {
+        require(submissions[index].deadline < now);
+        require(submissions[index].finals.length == 0);
+        submissions[index].previewApproved = false;
+        numberOfPreviewApprovals--;
+    }
+
+    function remove() public restricted {
+        require(numberOfPreviewApprovals == 0);
         creator.transfer(reward);
+        //selfdestruct, men detta måste göras noggrant
+    }
+
+    function getFinal(uint i, uint j) public view returns (string) {
+        return submissions[i].finals[j];
     }
 
 }
