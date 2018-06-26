0250ca581200583936b2cf528cead57391b89f1d
johanforslund <johan-forslund@hotmail.com>
2018-06-26 20:01:39 +0200

    more tests (with time traveling)

--- test/ArtRequest.test.js
+++ test/ArtRequest.test.js
@@ -6,6 +6,8 @@ const web3 = new Web3(ganache.provider({ gasLimit: 5800000 }));
 const compiledFactory = require('../ethereum/build/RequestFactory.json');
 const compiledRequest = require('../ethereum/build/Request.json');
 
+const NUM_SEC_DAY = 86400;
+
 let accounts;
 let factory;
 let requestAddress;
@@ -123,6 +125,61 @@ describe('Requests', () => {
 
     assert((balanceAfter - balanceBefore) < 0.01);
   });
+
+  it('is possible to cancel preview approval after 3 days', async () => {
+    await submitAndApprovePreview();
+    await timeTravel(NUM_SEC_DAY * 4);
+    await mineBlock();
+
+    await request.methods.cancelPreviewApproval(0).send({
+      from: accounts[0],
+      gas: '3000000'
+    });
+
+    const anyPreviewApproved = await request.methods.anyPreviewApproved().call();
+    assert(!anyPreviewApproved);
+  });
+
+  it('is possible to withdraw if requester has not approved/rejected final', async () => {
+    await submitAndApprovePreview();
+
+    await request.methods.submitFinal(0, 'http://final.com').send({
+      from: accounts[1],
+      value: web3.utils.toWei('1', 'ether'),
+      gas: '3000000'
+    });
+
+    await timeTravel(NUM_SEC_DAY * 4);
+    await mineBlock();
+
+    let balanceBefore = await web3.eth.getBalance(accounts[1]);
+    balanceBefore = web3.utils.fromWei(balanceBefore, 'ether');
+    balanceBefore = parseFloat(balanceBefore);
+
+    await request.methods.withdrawNoApproval(0).send({
+      from: accounts[1],
+      gas: '3000000'
+    });
+
+    let balanceAfter = await web3.eth.getBalance(accounts[1]);
+    balanceAfter = web3.utils.fromWei(balanceAfter, 'ether');
+    balanceAfter = parseFloat(balanceAfter);
+
+    assert((balanceAfter - balanceBefore > 4.99));
+  });
+
+  // it('time travels (ganache testing)', async () => {
+  //   const blockNumber = await web3.eth.getBlockNumber();
+  //   const block = await web3.eth.getBlock(blockNumber);
+  //   console.log(block.timestamp);
+  //
+  //   await timeTravel(NUM_SEC_DAY * 3);
+  //   await mineBlock();
+  //
+  //   const blockNumber2 = await web3.eth.getBlockNumber();
+  //   const block2 = await web3.eth.getBlock(blockNumber2);
+  //   console.log(block2.timestamp);
+  // });
 });
 
 
@@ -152,4 +209,32 @@ const submitAndApproveFinal = async () => {
     from: accounts[0],
     gas: '3000000'
   });
+}
+
+const timeTravel = function (time) {
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.sendAsync({
+      jsonrpc: "2.0",
+      method: "evm_increaseTime",
+      params: [time], // 86400 is num seconds in day
+      id: new Date().getTime()
+    }, (err, result) => {
+      if(err){ return reject(err) }
+      return resolve(result)
+    });
+  })
+}
+
+const mineBlock = function () {
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.sendAsync({
+      jsonrpc: "2.0",
+      method: "evm_mine",
+      params: [],
+      id: new Date().getTime()
+    }, (err, result) => {
+      if(err){ return reject(err) }
+      return resolve(result)
+    });
+  })
 }
